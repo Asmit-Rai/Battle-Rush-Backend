9428d53a093e9f4b7a3aaeea9a6e376b6801fda1
Asmit-Rai <211b075@juetguna.co.in>
2025-02-08 08:29:38 +0530

    first commit

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
-
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 
 const db = admin.firestore();
